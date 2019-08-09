89afa9d0b7c2563581420d1fa073d100665dd674
zhangyanling77 <18783226594@163.com>
2019-08-09 10:18:17 +0800

    Create rrd.Redirect.js

---
+++ rrd.Redirect.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Consumer } from './context'
+
+export default class Redirect extends Component {
+  constructor() {
+    super()
+  }
+
+
+  render() {
+    return (
+      <Consumer>
+      {
+        state => {
+          state.history.push(this.props.to)
+          return null
+        }
+      }
+      </Consumer>
+    )
+  }
+}
