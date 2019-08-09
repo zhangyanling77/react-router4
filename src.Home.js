90c7e21a6e0b0eb0eed8940af8ac7f4dcf8fad03
zhangyanling77 <18783226594@163.com>
2019-08-09 10:13:15 +0800

    Create src.Home.js

---
+++ src.Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+export default class Home extends Component {
+  constructor() {
+    super()
+  }
+
+
+  render() {
+    // console.log(this.props)
+    return (
+      <div>Home</div>
+    )
+  }
+}
